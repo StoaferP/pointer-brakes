598568ee0634c1f58c98119236033576a9d886c2
ChrisArgyle <chrisisdiy@gmail.com>
2024-01-10 23:31:06 -0500

    test: refactor existing test for new behavior

--- tests/test_pointer_sim.py
+++ tests/test_pointer_sim.py
@@ -11,20 +11,21 @@ from .utils import distance_between_points, swipe_left, swipe_right, swipe_swirl
 
 # test touch swipe
 @pytest.mark.parametrize("swipe", [swipe_right, swipe_left, swipe_swirl])
-@pytest.mark.parametrize("idle_before", [[], TOUCH_IDLE])
-@pytest.mark.parametrize("idle_after", [[], TOUCH_IDLE])
-def test_touch_swipe(swipe, idle_before, idle_after):
-    sim = PointerMotionSim()
+# test simple touch-driven motion (ie. a finger swipe with optional leading idle)
+@pytest.mark.parametrize("swipe", [swipe_right, swipe_left, swipe_swirl, swipe_idle])
+@pytest.mark.parametrize("idle_before", [lambda: [], swipe_idle])
+def test_idle_then_touch_motion(swipe, idle_before):
+    sim = PointerMotionSim(31.337)
 
-    touch_data = idle_before + swipe() + idle_after
+    touch_data = idle_before() + swipe()
     for i in range(len(touch_data)):
         # do tick
         sim.tick(time.monotonic(), touch_data[i])
 
         # assert delta_pos is as expected
         if i == 0 or not touch_data[i] or not touch_data[i - 1]:
-            assert not sim.delta_position
+            assert sim.delta_position is None
             continue
 
-        assert sim.delta_position
+        assert sim.delta_position is not None
         assert sim.delta_position.len() == distance_between_points(touch_data[i - 1], touch_data[i])
