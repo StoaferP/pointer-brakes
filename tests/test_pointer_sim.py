01edfc9c06dcfe8d3c6171a72e4837317944b705
ChrisArgyle <chrisisdiy@gmail.com>
2024-01-11 01:35:44 -0500

    test: refactor for readability, reliability

--- tests/test_pointer_sim.py
+++ tests/test_pointer_sim.py
@@ -2,35 +2,32 @@ from __future__ import annotations
 
 import time
 from math import isclose
-from typing import Callable
 
 import pytest
 
 from pointer_brakes import PointerMotionSim
 
-from .utils import distance_between_points, get_delta_time, swipe_idle, swipe_left, swipe_right, swipe_swirl
+from .utils import (
+    distance_between_points,
+    get_delta_time,
+    prep_sim_for_rolling,
+    swipe_idle,
+    swipe_left,
+    swipe_right,
+    swipe_swirl,
+)
 
 
 # test touch motion that interrupts rolling motion
 def test_touch_interrupts_rolling():
-    # start rolling motion
-    a_brake = 1
+    # prep sim for pointer rolling
+    a_brake = 5
     sim = PointerMotionSim(a_brake)
-
-    # pick two arbitrary points and find the delta time which would result in a certain velocity
-    v12 = 4
-    p1 = (-32, 15)
-    p2 = (-28, 23)
-    t12 = get_delta_time(p1, p2, v12)
-
-    # run sim with contrived data
-    fake_t = time.monotonic()
-    sim.tick(fake_t, p1)
-    fake_t += t12
-    sim.tick(fake_t, p2)
+    prep_sim_for_rolling(sim, (-32, 15), (-28, 23), 100)
 
     # run sim with touch idle to start pointer rolling motion
-    fake_t += 1
+    assert sim.state.timestamp is not None
+    fake_t = sim.state.timestamp + 1
     sim.tick(fake_t)
 
     # interrupt with touch data
@@ -52,64 +49,56 @@ def test_touch_interrupts_rolling():
     assert isclose(sim.delta_position.len(), distance_between_points(p3, p4))
 
 
-# test rolling motion (motion which continues after touch-motion, for example, a finger swipe then idle)
-@pytest.mark.parametrize("swipe", [swipe_right, swipe_left, swipe_swirl])
-def test_touch_motion_then_idle(swipe: Callable[[], list[tuple[int, int]]]):
-    # instantiate sim while specifying braking acceleration
-    a_brake = 1
+# rolling motion stops when new velocity magnitude drops below (or at) 0
+def test_rolling_motion_stops_eventually():
+    # prep sim for rolling
+    a_brake = 2
     sim = PointerMotionSim(a_brake)
+    v_prep = 20
+    prep_sim_for_rolling(sim, (-120, -84), (-23, -39), v_prep)
 
-    # construct touch data
-    touch_data = swipe()
-    final_pos = touch_data.pop()
+    # run sim with touch idle until we expect pointer motion to stop
+    delta_time = v_prep / a_brake  # found by solving v=v0-a*t when v=0
+    assert sim.state.timestamp is not None
+    sim.tick(sim.state.timestamp + delta_time + 0.1)  # fudge 100ms because floating point accuracy
 
-    # tick through touch data
-    for pos in touch_data:
-        sim.tick(time.monotonic(), pos)
+    # assert motion is stopped
+    assert sim.velocity is None
 
-    # ensure final pointer velocity is above braking acceleration
-    final_v = 4
-    delta_time = get_delta_time(touch_data[-1], final_pos, final_v)
-    time.sleep(delta_time)
-    sim.tick(time.monotonic(), final_pos)
 
-    # tick through idle data
-    t_idle = time.monotonic()
-    for pos in swipe_idle():
-        v0 = sim.velocity
+# rolling motion correctly calculates velocity and delta position
+def test_rolling_motion_correct_calculations():
+    # prep sim for rolling
+    a_brake = 7
+    sim = PointerMotionSim(a_brake)
+    v_prep = 48
+    prep_sim_for_rolling(sim, (12, 3), (-9, 25), v_prep)
 
-        # simulate a slow (embedded?) device by adding 20ms each tick
-        t_idle += 0.2
-        sim.tick(t_idle, pos)
+    # run sim with touch idle to so some rolling
+    delta_time = 2
+    assert sim.state.timestamp is not None
+    sim.tick(sim.state.timestamp + delta_time)
 
-        # we don't check anything if motion is stopped
-        if sim.state.timestamp is None or sim.last_state.timestamp is None:
-            continue
+    # assert velocity and delta_position are calculated as we expect
+    assert sim.velocity
+    assert isclose(sim.velocity.len(), v_prep - a_brake * delta_time)
+    assert sim.delta_position
+    # lets tolerate 1e-06 cuz floating point accuracy
+    assert isclose(sim.velocity.len(), v_prep * delta_time - a_brake / 2 * delta_time**2, rel_tol=6)
 
-        # calculate delta time
-        delta_time = sim.state.timestamp - sim.last_state.timestamp
 
-        # assert velocity matches our expectation
-        v_mag = v0.len() - a_brake * delta_time if v0 else 0
+# when motion is stopped velocity and delta_position are None
+def test_motion_stopped_then_no_calculations():
+    # prep for rolling
+    sim = PointerMotionSim(13)
+    prep_sim_for_rolling(sim, (23, -32), (2, -1), 2)
 
-        # we expect None if velocity magnitude would have stopped or if motion is halted
-        if v0 is None or v_mag <= 0:
-            assert sim.velocity is None
+    # manually stop motion
+    sim.stop_motion()
 
-        else:
-            assert sim.velocity is not None
-            assert isclose(sim.velocity.len(), v_mag)
-
-        # assert delta pos matches our expectation
-
-        # if motion is halted we expect None
-        if sim.velocity is None:
-            assert sim.delta_position is None
-        # otherwise we expect the normal accelerated motion calculation
-        else:
-            assert sim.delta_position is not None
-            assert v0 is not None
-            assert isclose(sim.delta_position.len(), v0.len() * delta_time - a_brake * delta_time**2, rel_tol=6)
+    # assert velocity and delta_postion are None
+    assert sim.velocity is None
+    assert sim.delta_position is None
 
 
 # test simple touch-driven motion (ie. a finger swipe with optional leading idle)
